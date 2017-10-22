8edb95c6213d7a40825e994237a91fe087673f43
qwales1 <qwales1@gmail.com>
2017-10-21 17:44:01 -0700

    fix it statements

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ niv.install('firebase@4.1.0');
 niv.install('firebase@4.2.0');
 
 describe('Firebase Version 4.0.0', () => {
-  it('database is a function', () => {
+  it('firebase/database exports a function', () => {
     const database = niv.require('firebase@4.0.0/database');
     assert.strictEqual(typeof database, 'function');
   });
 });
 
 describe('Firebase Version 4.1.0', () => {
-  it('database is a function', () => {
+  it('firebase/database exports a function', () => {
     const database = niv.require('firebase@4.1.0/database');
     assert.strictEqual(typeof database, 'function');
   });
 });
 
 describe('Firebase Version 4.2.0', () => {
-  it('database is a function', () => {
+  it('firebase/database exports a function', () => {
     const database = niv.require('firebase@4.2.0/database');
     assert.strictEqual(typeof database, 'function');
   });
