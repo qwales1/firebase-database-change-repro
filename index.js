f4a2b2480a6eddf666e1127543fbd7a494527878
qwales1 <qwales1@gmail.com>
2017-10-21 17:30:50 -0700

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+const niv = require('npm-install-version');
+const assert = require('assert');
+niv.install('firebase@4.0.0');
+niv.install('firebase@4.1.0');
+niv.install('firebase@4.2.0');
+
+describe('Firebase Version 4.0.0', () => {
+  it('database is a function', () => {
+    const database = niv.require('firebase@4.0.0/database');
+    assert.strictEqual(typeof database, 'function');
+  });
+});
+
+describe('Firebase Version 4.1.0', () => {
+  it('database is a function', () => {
+    const database = niv.require('firebase@4.1.0/database');
+    assert.strictEqual(typeof database, 'function');
+  });
+});
+
+describe('Firebase Version 4.2.0', () => {
+  it('database is a function', () => {
+    const database = niv.require('firebase@4.2.0/database');
+    assert.strictEqual(typeof database, 'function');
+  });
+});
